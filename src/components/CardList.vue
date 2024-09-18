18a25ae3136d111a2fc5d45bca2ac8f251083d22
jacopolegovini <legovini96@gmail.com>
2024-09-18 15:46:45 +0200

    function api created

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -4,8 +4,21 @@ import SingleCard from './SingleCard.vue';
 export default {
   data() {
     return {
-      apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0'
+        cards: [],
+        apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0'
     }
+  },
+  methods: {
+    addApi(){
+        axios.get(this.apiUrl)
+            .then((response) => {
+                console.log(response.data.data[0].id)
+                return this.cards
+            });
+    } 
+  },
+  mounted() {
+        this.addApi();
   },
   components: {
     SingleCard
@@ -16,7 +29,7 @@ export default {
 <template>
     <main>
         <h2>Number of cards</h2>
-        <SingleCard/>
+        <SingleCard v-for="card in cards"/>
     </main>
 </template>
 
