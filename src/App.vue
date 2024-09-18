5df940cb0f0316664a3ed23fc09c356cbf263365
jacopolegovini <legovini96@gmail.com>
2024-09-18 15:15:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+  import { MagicString } from 'vue/compiler-sfc';
+import AppMain from './components/AppMain.vue';
+
+  export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppMain/>
+</template>
+
+<style lang="scss">
+  @use "./styles/general.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <h1>test</h1>
+</template>
+
+<style scoped>
+</style>
